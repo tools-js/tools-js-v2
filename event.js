58f09f73c102ec97fd2e780b0d7100951087c0b7
Vihan B <contact@vihan.org>
2016-09-17 16:19:19 -0700

    Added data gathering and error paging

--- event.js
+++ event.js
@@ -1,11 +1,12 @@
 (function() {
     // Constants
     var PROJECT_TYPES = { NODEJS: 1, BROWSER: 2 };
+    var CUR_PAGE;
 
     var ToolsJS = {
         Project: {
             Name: "My Project",
-            Description: "My Project",
+            Desc: "My Project",
             Type: { Type: null, Image: null }
         },
 
@@ -20,7 +21,10 @@
                 self.removeClass("Hidden");
                 self.fadeIn(100);
                 
+                $(".Page").not(self).not('#' + CUR_PAGE).hide();
                 $(".Page").not(self).addClass("Hidden").fadeOut(100);
+
+                CUR_PAGE = "Page-" + name;
             }
         },
 
@@ -36,11 +40,27 @@
 
             // Update name/desc
             [ '#SubmitDetails', 'click', function() {
-                var name = $("#P-Name").val();
-                var desc = $("#P-Desc").val();
+                var name = $("#P-Name").val().trim();
+                var desc = $("#P-Desc").val().trim();
 
-                ToolsJS.Project.Name = name;
-                ToolsJS.Project.Desc = desc;
+                if (name) {
+                    ToolsJS.Project.Name = name;
+                    $(".Proj-Title").html(name);
+                    $("#P-Name").removeClass("error");
+                } else {
+                    $("#P-Name").addClass("error");
+                }
+
+                if (desc) {
+                    ToolsJS.Project.Desc = desc;
+                    $("#P-Desc").removeClass("error");
+                } else {
+                    $("#P-Desc").addClass("error");
+                }
+
+                if (name && desc) {
+                    ToolsJS.set.Screen("Module");
+                }
             } ],
 
             // Selected Project type
@@ -63,6 +83,27 @@
 
     // Dispatch events
     window.onload = function() {
+        // Set current page
+        CUR_PAGE = $(".Page").first().attr('id');
+
+        // Get data.json
+        $.ajax({
+            type: 'GET',
+            url: './data.json',
+            dataType: 'json',
+
+            success: function(data) {
+                ToolsJS.Data = data;
+                ToolsJS.set.Screen("Welcome");
+            },
+
+            error: function(data) {
+                var code = data.status || 404;
+                $(".Error-Code").html(code);
+                ToolsJS.set.Screen("Error");
+            }
+        });
+
         for (var i = 0; i < ToolsJS.event.length; i++) {
             var data = ToolsJS.event[i];
             $(data[0]).on(data[1], data[2]);
